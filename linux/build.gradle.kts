0652f5cfb82d3796c5535ef27896589c51905104
developer <devdjango@icloud.com>
2025-03-26 16:25:39 +0100

    Update build.gradle.kts

--- linux/build.gradle.kts
+++ linux/build.gradle.kts
@@ -1,11 +1,30 @@
 plugins {
     kotlin("jvm")
-    id("lightzone.java-conventions")
+    application
+    java
 }
+
 dependencies {
     implementation(project(":lightcrafts"))
     implementation("javax.help:javahelp:2.0.05")
+    
+    // Add potential test and compile dependencies
+    testImplementation("junit:junit:4.13.2")
+    implementation(kotlin("stdlib-jdk8"))
 }
+
 application {
     mainClass.set("com.lightcrafts.platform.linux.LinuxLauncher")
+}
+
+// Add some standard Java conventions
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
